1a9f0a46e8c6e2cee4620d8f32779b491fc63d59
Abhishek Rawat <shubhamrajvanshi60@gmail.com>
2024-04-01 12:25:18 +0530

    login api created

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const {registerController} = require("../controllers/authController")
+const {registerController,loginController} = require("../controllers/authController")
 
 const router = express.Router()
 
 // routes
+// Register || POST
 router.post('/register',registerController)
+// Register || LOGIN
+router.post('/login',loginController)
 
 module.exports = router
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors())
 app.use(morgan('dev'))
 
 
-// test routes
+// routes
 app.use('/api/v1/test',require('./routes//testRoutes'));
 app.use('/api/v1/auth',require('./routes/authRoutes'))
 
